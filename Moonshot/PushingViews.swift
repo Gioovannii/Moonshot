92de7c8ff3e541262ce988968e7b8296e27b0423
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-19 10:50:18 +0800

    New file for pushing views

---
+++ Moonshot/PushingViews.swift
@@ -0,0 +1,20 @@
+//
+//  PushingViews.swift
+//  Moonshot
+//
+//  Created by Giovanni Gaffé on 2021/10/19.
+//
+
+import SwiftUI
+
+struct PushingViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PushingViews_Previews: PreviewProvider {
+    static var previews: some View {
+        PushingViews()
+    }
+}
