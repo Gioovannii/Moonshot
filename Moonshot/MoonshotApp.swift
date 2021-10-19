6d8100b8cf755a051af9453b8b40fe7a43aba4cc
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-19 10:21:33 +0800

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Giovanni Gaffé on 2021/10/19.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
