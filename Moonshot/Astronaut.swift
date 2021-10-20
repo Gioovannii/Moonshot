e6f122ba335a842b234bf7daff7711cd24d141a2
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-20 15:10:29 +0800

    refactor files

--- Moonshot/Astronaut.swift
+++ Moonshot/Astronaut.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
