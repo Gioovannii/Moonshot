be7c4c368c97b2ad24252946b1b6f55b523a79fd
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-21 10:46:15 +0800

    set struct

--- Moonshot/MissionView.swift
+++ Moonshot/MissionView.swift
@@ -8,5 +8,21 @@
 import SwiftUI
 
 struct MissionView: View {
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
+    
    
+    }
+}
+
+struct MissionView_Previews: PreviewProvider {
+    
+    static let missions: [Mission] = Bundle.main.decode("missions.json")
+    static let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    static var previews: some View {
+        MissionView(mission: missions[0], astronauts: astronauts)
+    }
 }
