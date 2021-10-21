c547ed208cf299091064dc00a2ad47d072a468c5
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-21 10:46:56 +0800

    set layout for mission view

--- Moonshot/MissionView.swift
+++ Moonshot/MissionView.swift
@@ -13,6 +13,55 @@ struct MissionView: View {
         let astronaut: Astronaut
     }
     
+    let mission: Mission
+    let astronauts: [CrewMember]
+    
+    var body: some View {
+        
+        GeometryReader { geometry in
+            ScrollView(.vertical) {
+                VStack {
+                    Image(self.mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: geometry.size.width * 0.7)
+                        .padding(.top)
+                    
+                    Text(self.mission.description)
+                        .padding()
+                    
+                    ForEach(self.astronauts, id: \.role) { crewMember in
+                        HStack {
+                            Image(crewMember.astronaut.id)
+                                .resizable()
+                                .frame(width: 83, height: 60)
+                        }
+                        
+                    }
+                    
+                    Spacer(minLength: 25)
+                }
+            }
+        }
+        .navigationTitle(Text(mission.displayName))
+        .navigationBarTitleDisplayMode(.inline)
+    }
+    
+    init(mission: Mission, astronauts: [Astronaut]) {
+        self.mission = mission
+        
+        var matches = [CrewMember]()
+        
+        for member in mission.crew {
+            if let match = astronauts.first(where: { $0.id == member.name }) {
+                matches.append(CrewMember(role: member.role, astronaut: match))
+            } else  {
+                fatalError("Missing \(member)")
+            }
+        }
+        
+        self.astronauts = matches
+        
    
     }
 }
